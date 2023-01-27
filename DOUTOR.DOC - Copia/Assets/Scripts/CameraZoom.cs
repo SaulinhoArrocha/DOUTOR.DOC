a309dea250c63d3567f2f9ea7342427767ce63f3
SaulinhoArrocha <123772993+SaulinhoArrocha@users.noreply.github.com>
2023-01-27 17:34:33 -0300

    Doutor.doc update
    Arquivo do jogo atualizado.

---
+++ DOUTOR.DOC - Copia/Assets/Scripts/CameraZoom.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+    int zoom = 40;
+    float smooth = 5;
+    int normal = 90;
+
+    private bool isZoomed = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            isZoomed = true;
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            isZoomed = false;
+        }
+
+        if (isZoomed)
+        {
+            GetComponent<Camera>().fieldOfView = Mathf.Lerp(GetComponent<Camera>().fieldOfView, zoom, Time.deltaTime * smooth);
+
+
+        }
+        else
+        {
+            GetComponent<Camera>().fieldOfView = Mathf.Lerp(GetComponent<Camera>().fieldOfView, normal, Time.deltaTime * smooth);
+        }
+    }
+}
---
+++ DOUTOR.DOC - Copia/Assets/Scripts/RotacaVertical.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotacaVertical : MonoBehaviour
+{
+    private Rigidbody Cabeca;
+    public float verticalSpeed;
+    [SerializeField()] Vector3 rotacion = new Vector3(0, 0, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cabeca = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Rotate(-verticalSpeed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Rotate(verticalSpeed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.rotation = Quaternion.Euler(rotacion);
+        }
+
+    }
+}
