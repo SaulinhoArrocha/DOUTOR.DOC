e6bbeb156ae834d64ef760fcc43e2998854d807f
SaulinhoArrocha <123772993+SaulinhoArrocha@users.noreply.github.com>
2023-04-11 17:37:16 -0300

    Atualização 11/04/2023
    Otimização do PuzzleONE;
    está quase completo

--- DOUTOR.DOC atualizado/Assets/Scripts/ScriptsPuzzleONE/Rotacao.cs
+++ DOUTOR.DOC atualizado/Assets/Scripts/ScriptsPuzzleONE/Rotacao.cs
@@ -16,23 +16,23 @@ public class Rotacao : MonoBehaviour
     {
         currentRot = GetComponent<Transform>().eulerAngles;
 
-        if ((Input.GetAxis("Horizontal") > .2) && (currentRot.z <= 8 || currentRot.z >= 350))
+        if ((Input.GetAxis("Horizontal") > .2) && (currentRot.z <= 10 || currentRot.z >= 348))
         {
             transform.Rotate(0, 0, 1);
         }
 
-        if ((Input.GetAxis("Horizontal") < -.2) && (currentRot.z >= 351 || currentRot.z <= 9))
+        if ((Input.GetAxis("Horizontal") < -.2) && (currentRot.z >= 349 || currentRot.z <= 11))
         {
             transform.Rotate(0, 0, -1);
         }
 
 
-        if (Input.GetAxis("Vertical") > .2)
+        if ((Input.GetAxis("Vertical") > .2) && (currentRot.x <= 10 || currentRot.x >= 348))
         {
             transform.Rotate(1, 0, 0);
         }
 
-        if (Input.GetAxis("Vertical") < -.2)
+        if ((Input.GetAxis("Vertical") < -.2) && (currentRot.x >= 349 || currentRot.x <= 11))
         {
             transform.Rotate(-1, 0, 0);
         }
---
+++ DOUTOR.DOC atualizado/Assets/Scripts/ScriptsPuzzleONE/ball.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour
+{
+    public Transform pauseMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "Destino")
+        {
+            if (pauseMenu.gameObject.activeSelf)
+            {
+                pauseMenu.gameObject.SetActive(false);
+                Time.timeScale = 1;
+            }
+            else
+            {
+                pauseMenu.gameObject.SetActive(true);
+                Time.timeScale = 0;
+            }
+        }
+    }
+}
