063640c7fe39c50630f98cd715dfd143d8bc7578
joao123regis <119495289+joao123regis@users.noreply.github.com>
2023-04-21 09:02:40 -0300

    Atualização 21/04
    Adicionei a maleta do inventário e o sistema de coleta.

---
+++ DOUTOR.DOC atualizado/Assets/Scripts/Solid Inventory/ColetarObjeto.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColetarObjeto : MonoBehaviour
+{
+    public GameObject ativarObjeto; 
+
+    void OnMouseDown()
+    {
+        Destroy(gameObject); 
+        ativarObjeto.SetActive(true); 
+    }
+}
